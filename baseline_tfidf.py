893ffa8e20f2df325247f1a526bd4ff074aee871
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2019-04-18 16:03:54 +0200

    Add baseline_tfidf.py

---
+++ baseline_tfidf.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import warnings
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_distances
+
+
+def read_explanations(path):
+    header = []
+    uid = None
+
+    df = pd.read_csv(path, sep='\t')
+
+    for name in df.columns:
+        if name.startswith('[SKIP]'):
+            if 'UID' in name and not uid:
+                uid = name
+        else:
+            header.append(name)
+
+    if not uid or len(df) == 0:
+        warnings.warn('Possibly misformatted file: ' + path)
+        return []
+
+    return df.apply(lambda r: (r[uid], ' '.join(str(s) for s in list(r[header]) if not pd.isna(s))), 1).tolist()
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tables')
+    parser.add_argument('questions', type=argparse.FileType('r', encoding='UTF-8'))
+    args = parser.parse_args()
+
+    explanations = []
+
+    for path, _, files in os.walk(args.tables):
+        for file in files:
+            explanations += read_explanations(os.path.join(path, file))
+
+    df_e = pd.DataFrame(explanations, columns=('uid', 'text'))
+    df_q = pd.read_csv(args.questions, sep='\t')
+
+    vectorizer = TfidfVectorizer()
+    X_e = vectorizer.fit(df_q['Question']).fit_transform(df_e['text'])
+    X_q = vectorizer.transform(df_q['Question'])
+    X_dist = cosine_distances(X_q, X_e)
+
+    for i_question, distances in enumerate(X_dist):
+        for i_explanation in np.argsort(distances)[:20]:
+            print('{}\t{}'.format(df_q.loc[i_question]['questionID'], df_e.loc[i_explanation]['uid']))
+
+
+if '__main__' == __name__:
+    main()
